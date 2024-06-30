53de978d15771af83eee1f86f37b96aca6ab9c06
FrancisKpentey <fnkpentey@st.ug.edu.gh>
2024-06-30 09:51:54 +0000

    Task 2 arrayManipulation.js formatArrayStrings

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -5,9 +5,32 @@ function processArray(arr) {
         if (num % 2 === 0) {
             processedArray.push(num * num);
         } else {
-          processedArray.push(num * 3);
+            processedArray.push(num * 3);
         }
     }
     return processedArray;
 }
 
+function formatArrayStrings(strArr, numArr) {
+    let formattedArray = [];
+    
+    // Checking if strArr and numArr have the same length
+    if (strArr.length !== numArr.length) {
+        throw new Error('Arrays must have the same length');
+    }
+    for (let i = 0; i < strArr.length; i++) {
+        let str = strArr[i];
+        let num = numArr[i];
+        
+        if (num % 2 === 0) {
+            formattedArray.push(str.toUpperCase()); // Capitalize if even
+        } else {
+            formattedArray.push(str.toLowerCase()); // Lowercase if odd
+        }
+    }
+    return formattedArray;
+}
+module.exports = {
+    processArray,
+    formatArrayStrings
+};
